c60a3a55a91c86b1dcf0f110d11486bada47646c
msantamaria <msantamaria@mvdcreativestudio.com>
2022-08-30 16:16:59 -0300

    Carouseles y header listos

---
+++ assets/js/v2.js
@@ -0,0 +1,7 @@
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl));
+
+//Carrousel
+
+
+
